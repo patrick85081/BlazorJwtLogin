37734bfe87090697817d7c9a58aff4e0cc1b21ef
Patrick <patrick85081@gmail.com>
2022-10-18 21:40:38 +0800

    add: Add Value Controller.

---
+++ Server/Controllers/ValueController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorJwtLogin.Server.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class ValueController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "aaa", "bbb" };
+    } 
+}
